59ac65d18426d884c2eb20a9dc4973bdadca9dc7
bondarev <alexander.i.bondarev@gmail.com>
2013-08-20 19:15:38 +0400

    Add test for plain object famili. Fix clean-up for object produced by famili.

---
+++ spec/features/plain_object_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+class PlainObject
+  attr_accessor :name
+end
+
+class PlainObjectFamili < Famili::Mother
+  name { 'Plain Object' }
+end
+
+describe PlainObjectFamili do
+  it 'should build plain object' do
+    described_class.build.name.should == 'Plain Object'
+  end
+end
